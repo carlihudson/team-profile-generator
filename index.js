953e4043598e2ff377f64e9893a4e8bdcd7fb515
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2022-12-15 23:15:00 -0800

    built general file and data structure

---
+++ index.js
@@ -0,0 +1,62 @@
+// modules required for index
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path')
+const generateHTML = require('generateHtML');
+
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter your name',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your employee ID?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is your email address?",
+        name: 'email',
+    },
+    
+    // employee specific questions
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: "What is your office number?",
+        name: 'officeNumber',
+    },
+    {
+        type: 'input',
+        message: "What school do you go to?",
+        name: 'school',
+    }
+]
+
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(inquirerResponses => {
+            writeToFile('./dist.index.html', 
+            generateHTML({...inquirerResponses}), (err) => err ? 
+            console.log(err) : console.log('success!'));
+            
+        });
+}
+
+
+
+// Function call to initialize app
+init()
